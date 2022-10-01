fca79445722b81072199fb75612da5ef8e74fc68
Tristan Bilot <tristan_bilot@carrefour.com>
2022-10-01 12:12:21 +0200

    ✅ add tests for inference

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
+import math
+
 from autodiff import *
+
 
 def test_expr_node1():
     node = ExprNode("2x")
@@ -36,3 +39,19 @@ def test_autodiff3():
     exp = "cos(cos(sin(exp(sin(4x)))))"
     diff = AutoDiff(exp)
     assert diff.differentiate() == "-sin(cos(sin(exp(sin(4x))))) * -sin(sin(exp(sin(4x)))) * cos(exp(sin(4x))) * exp(sin(4x)) * cos(4x) * 4"
+
+
+def test_inference1():
+    exp = "sin(exp(sin(x^9)))"
+    diff = AutoDiff(exp)
+    assert math.ceil(diff.differentiate(x=3)) == -14475
+
+def test_inference2():
+    exp = "cos(2x)"
+    diff = AutoDiff(exp)
+    assert round(diff.differentiate(x=2), 2) == 1.51
+
+def test_inference3():
+    exp = "cos(exp(sin(cos(cos(3x^9)))))"
+    diff = AutoDiff(exp)
+    assert round(diff.differentiate(x=5), 2) == 423175.18
